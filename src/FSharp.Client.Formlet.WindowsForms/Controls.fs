ecbd461cfc4efae62f5c3337111a0abd17da73fb
mrange <marten_range@hotmail.com>
2014-10-25 00:30:10 +0200

    Added initial WindowsForms

---
+++ src/FSharp.Client.Formlet.WindowsForms/Controls.fs
@@ -0,0 +1,44 @@
+﻿(* Copyright 1999-2005 The Apache Software Foundation or its licensors, as
+ * applicable.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *)
+
+namespace FSharp.Client.Formlet.WindowsForms
+
+open System.Windows.Forms
+
+open FSharp.Client.Formlet.Core
+
+module Controls =
+
+    let EmptyChangeNotification : FormletChangeNotification = fun () -> ()
+
+    type InputTextControl(initialText : string) as this =
+        inherit TextBox()
+
+        let mutable text                = initialText
+
+        do
+            this.Text <- initialText
+
+        member val ChangeNotifier = EmptyChangeNotification with get, set
+
+
+        override this.OnLostFocus (e) =
+            base.OnLostFocus(e)
+
+            if text <> this.Text then
+                text <- this.Text
+
+                this.ChangeNotifier ()
---
+++ src/FSharp.Client.Formlet.WindowsForms/FormletControl.fs
@@ -0,0 +1,17 @@
+﻿namespace FSharp.Client.Formlet.WindowsForms
+
+
+open System.Windows.Forms
+
+open FSharp.Client.Formlet.Core
+
+type FormletContext () =
+    interface IFormletContext with
+        member x.PushTag tag            = ()
+        member x.PopTag ()              = ()
+
+type FormletControl<'TValue> (submit : 'TValue -> unit, formlet : Formlet<FormletContext, Control, 'TValue>) as this =
+    inherit Control()
+    let x = 3
+
+
